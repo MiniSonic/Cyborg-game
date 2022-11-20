604250c38c977857e47baa3822897fd8248329e7
MiniSonic <vyelnino@gmail.com>
2022-11-20 14:50:05 -0800

    import view

---
+++ resources/views/layouts/reuse/footer.blade.php
@@ -0,0 +1,30 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights reserved.
+
+                    <br>Design: <a href="https://templatemo.com" target="_blank"
+                        title="free CSS templates">TemplateMo</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+<!-- Scripts -->
+<!-- Bootstrap core JavaScript -->
+<script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+
+<script src="{{asset('js/isotope.min.js')}}"></script>
+<script src="{{asset('js/owl-carousel.js')}}"></script>
+<script src="{{asset('js/tabs.js')}}"></script>
+<script src="{{asset('js/popup.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
+
+
+</body>
+
+</html>
